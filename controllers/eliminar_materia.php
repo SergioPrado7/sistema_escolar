d0bfac41bd365dac56a43d172e5dff83ab1ca29c
Daniel Benavente <daniel.benavente.22isc@tecsanpedro.edu.mx>
2026-03-25 18:36:15 -0600

    eliminar materia

--- controllers/eliminar_materia.php
+++ controllers/eliminar_materia.php
@@ -5,11 +5,37 @@ require_once '../config/database.php';
 if (isset($_GET['id'])) {
     $conexion = new Conexion();
     $db = $conexion->getConnection();
+    $id_materia = $_GET['id'];
+    
     try {
-        $stmt = $db->prepare("DELETE FROM materias WHERE id_materia = :id");
-        $stmt->execute([':id' => $_GET['id']]);
+        $db->beginTransaction();
+
+        $stmt1 = $db->prepare("DELETE FROM carga_academica WHERE id_horario IN (SELECT id_horario FROM horarios WHERE id_materia = :id)");
+        $stmt1->execute([':id' => $id_materia]);
+
+        $stmt2 = $db->prepare("DELETE FROM horarios WHERE id_materia = :id");
+        $stmt2->execute([':id' => $id_materia]);
+
+        $stmt3 = $db->prepare("DELETE FROM grupos WHERE id_materia = :id");
+        $stmt3->execute([':id' => $id_materia]);
+
+        $stmt4 = $db->prepare("DELETE FROM materias WHERE id_materia = :id");
+        $stmt4->execute([':id' => $id_materia]);
+        
+        $db->commit();
+        header("Location: ../views/gestion_academica.php");
+        exit();
+        
     } catch (PDOException $e) {
+        $db->rollBack();
+        echo "<script>
+                alert('Error del sistema al intentar aplicar el borrado en cascada: " . addslashes($e->getMessage()) . "'); 
+                window.location.href='../views/gestion_academica.php';
+              </script>";
+        exit();
     }
+} else {
+    header("Location: ../views/gestion_academica.php");
+    exit();
 }
-header("Location: ../views/gestion_academica.php");
-exit();
+?>
